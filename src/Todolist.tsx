ec49d3abec704798f6bc9f7745c71b972d459c92
Evgeny Morash <evgenymorash@gmail.com>
2022-07-02 10:39:24 +0300

    add task

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 import {FilterType, TasksType} from "./App";
 
 export type PropsType = {
@@ -7,10 +7,14 @@ export type PropsType = {
     changeFilterStatus: (filter: FilterType) => void
     removeTaskHandler: (id: string) => void
     changeStatusHandler: (id: string, isDone: boolean) => void
+    createTaskHandler: (title: string) => void
 
 }
 
 export function Todolist(props: PropsType) {
+
+    const [taskTitle, setTaskTitle] = useState<string>('')
+    const [errorTaskTitle, setErrorTaskTitle] = useState<boolean>(false)
 
     const onAllClickHandler = () => {
         props.changeFilterStatus('all')
@@ -24,10 +28,48 @@ export function Todolist(props: PropsType) {
         props.changeFilterStatus('completed')
     };
 
+    const onAddClickHandler = () => {
+        if (taskTitle.trim()) {
+        props.createTaskHandler(taskTitle.trim())
+        setTaskTitle('')
+        } else {
+            setErrorTaskTitle(true)
+        }
+    };
+
+    const onNewTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTaskTitle(e.currentTarget.value)
+        errorTaskTitle && setErrorTaskTitle(false)
+    };
+
+    const onEnterHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        // if (e.key === 'Enter') {
+        //     onAddClickHandler()
+        // }
+
+        (e.key === 'Enter') && onAddClickHandler()
+
+    };
+
+    const activeButtonStyle = {
+        backgroundColor: 'lightblue'
+    }
+
     return (
         <div>
 
             <h3>ToDo!</h3>
+            <div>
+                <input type="textarea"
+                       onChange={onNewTitleChangeHandler}
+                       value={taskTitle}
+                       onKeyDown={onEnterHandler}
+                />
+                <button onClick={onAddClickHandler}>Create</button>
+                <div style={{color: "red"}}>
+                    {errorTaskTitle && 'Invalid value!'}
+                </div>
+            </div>
             <ul>
                 {props.tasks.map((t) => {
 
@@ -40,7 +82,9 @@ export function Todolist(props: PropsType) {
                     };
 
                     return (
+
                         <li key={t.id} style={{listStyle: 'none'}}>
+
                             <input type="checkbox" checked={t.isDone} onChange={onchangeHandler}/>
                             <span>{t.title}</span>
                             <button style={{margin: 5}} onClick={onDeleteHandler}>X</button>
