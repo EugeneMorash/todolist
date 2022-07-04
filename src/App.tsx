3ddb10aca2b092d415aa84cc4548f6126806254e
Evgeny Morash <evgenymorash@gmail.com>
2022-07-04 14:34:12 +0300

    big state начальное состояние

--- src/App.tsx
+++ src/App.tsx
@@ -15,36 +15,52 @@ export type FilterType = 'all' | 'active' | 'completed'
 
 function App() {
 
-    const [tasks, setTasks] = useState([
+    // const [tasks, setTasks] = useState([
+    //     {id: v1(), title: 'Water', isDone: false},
+    //     {id: v1(), title: 'Milk', isDone: true},
+    //     {id: v1(), title: 'Beer', isDone: true},
+    //     {id: v1(), title: 'Lemonade', isDone: false},
+    //     {id: v1(), title: 'Wine', isDone: false},
+    //     {id: v1(), title: 'Juice', isDone: true},
+    // ])
+
+    const todolistID_1 = v1()
+    const todolistID_2 = v1()
+
+    const [filter, setFilter] = useState('all')
+
+    const [todolists, setTodolists] = useState([
+        {
+            todolistID: todolistID_1,
+            title: 'What to Eat',
+            filter: 'all'
+        },
+        {
+            todolistID: todolistID_2,
+            title: 'What to Drink',
+            filter: 'all'
+        },
+    ])
+
+    const [tasks, setTAsks] = useState<TasksType>({
+    [todolistID_1]: [
+        {id: v1(), title: 'Carrot', isDone: false},
+        {id: v1(), title: 'Meat', isDone: true},
+        {id: v1(), title: 'Cabbage', isDone: false},
+        {id: v1(), title: 'Ice Cream', isDone: true},
+        {id: v1(), title: 'Sausage', isDone: true},
+        {id: v1(), title: 'Soup', isDone: true},
+],
+    [todolistID_2]: [
         {id: v1(), title: 'Water', isDone: false},
         {id: v1(), title: 'Milk', isDone: true},
         {id: v1(), title: 'Beer', isDone: true},
         {id: v1(), title: 'Lemonade', isDone: false},
         {id: v1(), title: 'Wine', isDone: false},
         {id: v1(), title: 'Juice', isDone: true},
-    ])
+],
+})
 
-    const [filter, setFilter] = useState('all')
-
-//     const [tasks, setTAsks] = useState<TasksType>({
-//     [todolistID_1]: [
-//         {id: v1(), title: 'Carrot', isDone: false},
-//         {id: v1(), title: 'Meat', isDone: true},
-//         {id: v1(), title: 'Cabbage', isDone: false},
-//         {id: v1(), title: 'Ice Cream', isDone: true},
-//         {id: v1(), title: 'Sausage', isDone: true},
-//         {id: v1(), title: 'Soup', isDone: true},
-// ],
-//     [todolistID_2]: [
-//         {id: v1(), title: 'Water', isDone: false},
-//         {id: v1(), title: 'Milk', isDone: true},
-//         {id: v1(), title: 'Beer', isDone: true},
-//         {id: v1(), title: 'Lemonade', isDone: false},
-//         {id: v1(), title: 'Wine', isDone: false},
-//         {id: v1(), title: 'Juice', isDone: true},
-// ],
-//
-// })
     const changeStatusHandler = (id: string, isDone: boolean) => {
         setTasks(tasks.map(t => t.id === id ? {...t, isDone} : t))
     }
